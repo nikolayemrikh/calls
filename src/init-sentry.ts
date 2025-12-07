1447077c7cf91c3dd329cc16d0e471078f5fcce5
Nikolay Emrikh <redishko@gmail.com>
2025-12-07 18:39:41 +0300

    fix

--- src/init-sentry.ts
+++ src/init-sentry.ts
@@ -4,4 +4,5 @@ Sentry.init({
   dsn: 'https://91ba10c0ecaba65b10cbc5d27bea1da6@o4507922532794368.ingest.de.sentry.io/4510464101056592',
   sendDefaultPii: true,
   environment: import.meta.env.MODE,
+  tracesSampleRate: 1,
 });
