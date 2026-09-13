bf968e1ddc2799c4c242f1cc228a8b254e3e3a81
Nikolay Emrikh <redishko@gmail.com>
2026-09-13 20:06:01 +0300

    fix config

--- src/core/peer/getIceServers/index.ts
+++ src/core/peer/getIceServers/index.ts
@@ -1,7 +1,7 @@
-export const getIceServers = () => [
-  { url: 'stun:stun.l.google.com:19302' },
+export const getIceServers = (): RTCIceServer[] => [
+  { urls: 'stun:stun.l.google.com:19302' },
   {
-    url: `turns:${import.meta.env.VITE_TURN_SERVER_HOST}:${import.meta.env.VITE_TURN_SERVER_PORT}`,
+    urls: `turns:${import.meta.env.VITE_TURN_SERVER_HOST}:${import.meta.env.VITE_TURN_SERVER_PORT}`,
     username: import.meta.env.VITE_TURN_SERVER_USERNAME,
     credential: import.meta.env.VITE_TURN_SERVER_CREDENTIAL,
   },
