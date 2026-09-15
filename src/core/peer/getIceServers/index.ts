437bac6e0e6387c7aa6dfea6d19a9d7c3cfa2bfe
Nikolay Emrikh <redishko@gmail.com>
2026-09-15 22:31:13 +0300

    ice servers more urls

--- src/core/peer/getIceServers/index.ts
+++ src/core/peer/getIceServers/index.ts
@@ -1,7 +1,11 @@
 export const getIceServers = (): RTCIceServer[] => [
   { urls: 'stun:stun.l.google.com:19302' },
   {
-    urls: [`turn:${import.meta.env.VITE_TURN_SERVER_HOST}:${import.meta.env.VITE_TURN_SERVER_PORT}`],
+    urls: [
+      `turn:${import.meta.env.VITE_TURN_SERVER_HOST}:${import.meta.env.VITE_TURN_SERVER_PORT}`,
+      `turn:${import.meta.env.VITE_TURN_SERVER_HOST}:${import.meta.env.VITE_TURN_SERVER_PORT}?transport=tcp`,
+      `turns:${import.meta.env.VITE_TURN_SERVER_HOST}:${import.meta.env.VITE_TURN_S_SERVER_PORT}?transport=tcp`,
+    ],
     username: import.meta.env.VITE_TURN_SERVER_USERNAME,
     credential: import.meta.env.VITE_TURN_SERVER_CREDENTIAL,
   },
